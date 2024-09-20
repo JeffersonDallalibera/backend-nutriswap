4315efef2f512df4d44a86f94cb956e8b07a82f6
Jefferson <jeffersonmigueldallalibera@gmail.com>
2024-09-19 21:39:17 -0300

    ajuste back

--- Config.py
+++ Config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'chaveSecreta!@!eadTCCPYTHON')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://postgres:postgres@localhost/nutriswap?client_encoding=utf8')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://postgres:X6nAzV89y3mADbC0@pithily-sweeping-gerenuk.data-1.use1.tembo.io:5432/postgres')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
